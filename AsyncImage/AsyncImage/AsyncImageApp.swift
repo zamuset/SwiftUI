0d38e617e270b648fbcd313a98de62ccd232e521
Samuel Chavez <sam_tod@hotmail.com>
2024-05-18 19:09:27 -0600

    Simple `AsyncImage` demo

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Samuel Chavez on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImage/AsyncImage/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Samuel Chavez on 27/12/22.
+//
+
+import SwiftUI
+
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
+struct ContentView: View {
+    private let imageUrl = "https://credo.academy/credo-academy@3x.png"
+
+    var body: some View {
+//        BASIC
+//        AsyncImage(url: URL(string: imageUrl))
+//        SCALE
+//        AsyncImage(url: URL(string: imageUrl), scale: 3.0)
+//        PLACEHOLDER
+//        AsyncImage(url: URL(string: imageUrl)) { image in
+//            image
+//                .imageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill")
+//                .iconModifier()
+//        }
+//        .padding(40)
+//      PHASES
+//        AsyncImage(url: URL(string: imageUrl)) { phase in
+//            if let image = phase.image {
+//                image.imageModifier()
+//            } else if phase.error != nil {
+//                Image(systemName: "ant.circle.fill")
+//                    .iconModifier()
+//            } else {
+//                Image(systemName: "photo.circle.fill")
+//                    .iconModifier()
+//            }
+//        }
+//        .padding(40)
+
+//      ANIMATION
+        AsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .imageModifier()
+//                    .transition(.move(edge: .bottom))
+//                    .transition(.slide)
+                    .transition(.scale)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill")
+                    .iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill")
+                    .iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
