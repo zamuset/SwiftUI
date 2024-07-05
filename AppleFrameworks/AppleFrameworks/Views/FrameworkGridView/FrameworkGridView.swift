c3bbe6c4f3b4d28110184b25af4a4fb09692f6ca
Samuel Chavez <sam_tod@hotmail.com>
2024-07-04 19:25:50 -0600

    Add toogle action from grid to list

--- AppleFrameworks/AppleFrameworks/Views/FrameworkGridView/FrameworkGridView.swift
+++ AppleFrameworks/AppleFrameworks/Views/FrameworkGridView/FrameworkGridView.swift
@@ -12,6 +12,53 @@ struct FrameworkGridView: View {
     @StateObject var viewModel = FrameworkGridViewModel()
     
     var body: some View {
+        NavigationStack {
+            Group {
+                if viewModel.showList {
+                    List(MockData.frameworks) { framework in
+                        NavigationLink {
+                            FrameworkDetailView(framework: framework,
+                                                isShowingDetailView: $viewModel.isShowingDetail,
+                                                isShowingCloseBtn: .constant(false))
+                        } label: {
+                            FrameworkTitleView(framework: framework, showHorizontal: $viewModel.showList)
+                        }
+                    }
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: viewModel.columns) {
+                            ForEach(MockData.frameworks) { framework in
+                                NavigationLink(value: framework) {
+                                    FrameworkTitleView(framework: framework,
+                                                       showHorizontal: $viewModel.showList)
+                                }
+                            }
+                        } // LazyVGrid
+                        
+                    } // ScrollView
+                }
+            }
+            .navigationTitle("💻 Frameworks")
+            .navigationDestination(for: Framework.self) { framework in
+                FrameworkDetailView(framework: framework,
+                                    isShowingDetailView: $viewModel.isShowingDetail,
+                                    isShowingCloseBtn: !$viewModel.showList)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        viewModel.showList.toggle()
+                    }, label: {
+                        let imageName = viewModel.showList ? "square.grid.3x3" : "list.bullet"
+                        Image(systemName: imageName)
+                    })
+                }
+            }
+        }
+        .tint(Color(.label))
+        
+        // Deprecated
+        /*
         NavigationView {
             Group {
                 if viewModel.showList {
@@ -58,10 +105,18 @@ struct FrameworkGridView: View {
             })
         }
         .tint(Color(.label))
+        */
     }
 }
 
 #Preview {
     FrameworkGridView()
         .preferredColorScheme(.dark)
+}
+
+prefix func ! (value: Binding<Bool>) -> Binding<Bool> {
+    Binding<Bool>(
+        get: { !value.wrappedValue },
+        set: { value.wrappedValue = !$0 }
+    )
 }
--- AppleFrameworks/AppleFrameworks/Views/ReusableViews/FrameworkTitleView.swift
+++ AppleFrameworks/AppleFrameworks/Views/ReusableViews/FrameworkTitleView.swift
@@ -21,6 +21,7 @@ struct FrameworkTitleView: View {
                 Text(framework.name)
                     .font(.title)
                     .fontWeight(.semibold)
+                    .foregroundStyle(Color(.label))
                     .scaledToFit()
                     .minimumScaleFactor(0.6)
             }
