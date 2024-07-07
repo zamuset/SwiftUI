4c78e2ff24bd5eff774d3c005d565206897f681b
Samuel Chavez <sam_tod@hotmail.com>
2024-07-06 19:18:33 -0600

    Create EmptyState view

--- Appetizers/Appetizers/Views/OrderView.swift
+++ Appetizers/Appetizers/Views/OrderView.swift
@@ -9,29 +9,35 @@ import SwiftUI
 
 struct OrderView: View {
     
-    @State private var orderItems = MockData.appetizers
+    @State private var orderItems = MockData.appetizers.prefix(3)
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(orderItems, id: \.id) { appetizer in
-                        AppetizerItem(appetizer: appetizer)
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(orderItems, id: \.id) { appetizer in
+                            AppetizerItem(appetizer: appetizer)
+                        }
+                        .onDelete(perform: { indexSet in
+                            orderItems.remove(atOffsets: indexSet)
+                        })
                     }
-                    .onDelete(perform: { indexSet in
-                        orderItems.remove(atOffsets: indexSet)
-                    })
-                }
-                .listStyle(.plain)
-                
-                Spacer()
-                
-                Button(action: {
+                    .listStyle(.plain)
                     
-                }, label: {
-                    APButton(title: "$\(413.44, specifier: "%.2f") - Add to Order")
-                })
-                .padding(.bottom, 20)
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        APButton(title: "$\(413.44, specifier: "%.2f") - Add to Order")
+                    })
+                    .padding(.bottom, 20)
+                }
+                
+                if orderItems.isEmpty {
+                    EmptyState(imageName: "empty-order", message: "You have no items in your order")
+                }
             }
             .navigationTitle("🧾 Order")
         }
---
+++ Appetizers/Appetizers/Views/ReusableViews/EmptyState.swift
@@ -0,0 +1,37 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by Samuel Chavez on 06/07/24.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    let imageName: String
+    let message: String
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea(edges: .all)
+            
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.secondary)
+                    .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    EmptyState(imageName: "empty-order", message: "This is a test message")
+}
