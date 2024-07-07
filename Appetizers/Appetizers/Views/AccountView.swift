3d16f2bbfde854471818e06dc08cbe4738781f9e
Samuel Chavez <sam_tod@hotmail.com>
2024-07-06 20:06:40 -0600

    Add improvements for AccountView form, implement `@FocusState` and keyboar toolbar

--- Appetizers/Appetizers/Views/AccountView.swift
+++ Appetizers/Appetizers/Views/AccountView.swift
@@ -10,14 +10,30 @@ import SwiftUI
 struct AccountView: View {
     
     @State var viewModel = AccountViewModel()
+    @FocusState private var focusTextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section("Personal Info") {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusTextField, equals: .firstName)
+                        .onSubmit { focusTextField = .lastName }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusTextField, equals: .lastName)
+                        .onSubmit { focusTextField = .email }
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusTextField, equals: .email)
+                        .submitLabel(.continue)
+                        .onSubmit { focusTextField = nil }
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.none)
                     
@@ -31,13 +47,24 @@ struct AccountView: View {
                     })
                 }
                 
-                    Section("Requests") {
-                        Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
-                        Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefill)
-                    }
-                    .tint(.accentColor)
+                Section("Requests") {
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefill)
+                }
+                .tint(.accentColor)
             }
             .navigationTitle("🙃 Account")
+            .toolbar(content: {
+                ToolbarItemGroup(placement: .keyboard) {
+                    HStack {
+                        Spacer()
+                        Button("Dismiss") {
+                            focusTextField = nil
+                        }
+                    }
+                    
+                }
+            })
             .task {
                 viewModel.retrieveUser()
             }
